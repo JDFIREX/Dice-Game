41d8ad3ee78b81ed1221f67b39abc8ea6d301f3f
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-02-19 22:24:35 +0100

    .

--- app/index.js
+++ app/index.js
@@ -203,4 +203,230 @@ function settingsChange(e){
 
 
 
+
+
 // zacznięcie gry 
+
+
+
+
+let start = document.querySelector(".btn_start");
+let user1 = document.querySelector(".user_1");
+
+let user1Points = {
+    jedynki : 0,
+    dwojki : 0,
+    trojki : 0,
+    czworki : 0,
+    piatki : 0,
+    szostki : 0,
+    sumax1 : 0,
+    trzyx : 0,
+    czteryx : 0,
+    full : 0,
+    maly: 0,
+    duzy: 0,
+    general : 0,
+    szansa : 0,
+    sumax2: 0,
+    razem : 0,
+}
+
+let user2Points = {
+    jedynki : 0,
+    dwojki : 0,
+    trojki : 0,
+    czworki : 0,
+    piatki : 0,
+    szostki : 0,
+    sumax1 : 0,
+    trzyx : 0,
+    czteryx : 0,
+    full : 0,
+    maly: 0,
+    duzy: 0,
+    general : 0,
+    szansa : 0,
+    sumax2: 0,
+    razem : 0,
+}
+
+let user3Points = {
+    jedynki : 0,
+    dwojki : 0,
+    trojki : 0,
+    czworki : 0,
+    piatki : 0,
+    szostki : 0,
+    sumax1 : 0,
+    trzyx : 0,
+    czteryx : 0,
+    full : 0,
+    maly: 0,
+    duzy: 0,
+    general : 0,
+    szansa : 0,
+    sumax2: 0,
+    razem : 0,
+}
+
+let user4Points = {
+    jedynki : 0,
+    dwojki : 0,
+    trojki : 0,
+    czworki : 0,
+    piatki : 0,
+    szostki : 0,
+    sumax1 : 0,
+    trzyx : 0,
+    czteryx : 0,
+    full : 0,
+    maly: 0,
+    duzy: 0,
+    general : 0,
+    szansa : 0,
+    sumax2: 0,
+    razem : 0,
+}
+
+start.addEventListener("click", (e) => {
+    
+    StartGame(user1)
+
+})
+
+function StartGame(user){
+
+    let Game = new Promise(resolve => {
+        let score = FirstRole(user)
+
+        resolve(score)
+    })
+
+    Game.then(r => {
+
+        CountScore(r,user)
+
+    })
+
+}
+
+
+
+
+function FirstRole(user){
+
+    // dla podanego user powinno sie pobrac jego kostki i wykonać wylosowanie
+    // zastosowanie Math.random do wylosowania 1 - 5 liczby potem wykonanie animacji która obróci każdą kostkę na daną stronę gdzie
+    // znajduje sie liczba oczek taka jaka została wylosowana za pomocą switch pod sam koniec w tablicy zwraca wylosowane kostki
+    // w kolejnosci od lewej -> lewa 1 korstka = 1 losowy numer 
+
+}
+
+// let s = [];
+
+// for(let i = 0; i < 5 ; i++){
+//     let r = Math.ceil(Math.random() * 6)
+//     s.push(r)
+// }
+
+// CountScore(s,user1)
+
+
+function CountScore(score,user){
+
+    user = user.dataset.user;
+
+    let tablica = document.querySelectorAll(`[data-player="${user}"]`);
+
+    console.log(tablica)
+
+    let jedynki = score.filter(i => i == 1)
+    jedynki != 0 ? jedynki = jedynki.reduce((a,b) => a + b) : jedynki = 0;
+
+    let dwojki = score.filter(i => i == 2)
+    dwojki != 0 ? dwojki = dwojki.reduce((a,b) => a + b) : dwojki = 0;
+
+    let trojki = score.filter(i => i == 3)
+    trojki != 0 ? trojki = trojki.reduce((a,b) => a + b) : trojki = 0;
+
+    let czworki = score.filter(i => i == 4)
+    czworki != 0 ? czworki = czworki.reduce((a,b) => a + b) : czworki = 0;
+
+    let piatki = score.filter(i => i == 5)
+    piatki != 0 ? piatki = piatki.reduce((a,b) => a + b) : piatki = 0;
+
+    let szostki = score.filter(i => i == 6)
+    szostki != 0 ? szostki = szostki.reduce((a,b) => a + b) : szostki = 0;
+
+    let trzyx = 0;
+    let czteryx = 0;
+    let full = 0;
+
+        let set = new Set(score)
+        let t = []
+        set.forEach(s => t.push(s))
+
+        if(t.length <= 3){
+            t.map(x => {
+
+                let c = 0;
+
+                score.map(i => {
+                    if(i == x){
+                        c++;
+                    }
+                })
+                if(t.length == 2 && c == 3){
+                    full = 25;
+                }
+
+                if(c == 3){
+                    trzyx = score.reduce((a,b) => a + b) 
+                }else if(c == 4){
+                    czteryx = score.reduce((a,b) => a + b) 
+                }
+
+            })
+        }
+
+    let maly = 0;
+    let duzy = 0;
+
+        if(t.length >= 4){
+
+            let sort = [...t]
+            sort = sort.sort((a,b) => a - b)
+            
+            let malycheck = [
+                [sort.includes(1),sort.includes(2),sort.includes(3),sort.includes(4)].every(a => a),
+                [sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5)].every(a => a),
+                [sort.includes(3),sort.includes(4),sort.includes(5),sort.includes(6)].every(a => a),
+            ];
+
+            let duzycheck = [
+                [sort.includes(1),sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5)].every(a => a),
+                [sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5),sort.includes(6)].every(a => a),
+            ];
+
+            if(malycheck.some(a => a)){
+                maly = 30;
+            }
+            if(duzycheck.some(a => a)){
+                duzy = 40;
+            }
+            
+        }
+
+    let general = 0;
+
+        if(t.length == 1){
+
+            general = 50;
+
+        }
+
+     let szansa = score.reduce((a,b) => a + b);
+
+    console.log(score,jedynki,dwojki,trojki,czworki,piatki,szostki,trzyx,czteryx,full,maly,duzy,general,szansa)
+}
