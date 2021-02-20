a3aac11e3b32cecfd094c4c61f6902b7d02dbfe6
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-02-20 16:09:22 +0100

    .

--- app/index.js
+++ app/index.js
@@ -212,82 +212,242 @@ function settingsChange(e){
 
 let start = document.querySelector(".btn_start");
 let user1 = document.querySelector(".user_1");
+let user2 = document.querySelector(".user_2");
 
 let user1Points = {
-    jedynki : 0,
-    dwojki : 0,
-    trojki : 0,
-    czworki : 0,
-    piatki : 0,
-    szostki : 0,
+    jedynki : {
+        score : 0,
+        confirm : false
+    },
+    dwojki : {
+        score : 0,
+        confirm : false
+    },
+    trojki : {
+        score : 0,
+        confirm : false
+    },
+    czworki : {
+        score : 0,
+        confirm : false
+    },
+    piatki : {
+        score : 0,
+        confirm : false
+    },
+    szostki : {
+        score : 0,
+        confirm : false
+    },
     sumax1 : 0,
-    trzyx : 0,
-    czteryx : 0,
-    full : 0,
-    maly: 0,
-    duzy: 0,
-    general : 0,
-    szansa : 0,
+    trzyx : {
+        score : 0,
+        confirm : false
+    },
+    czteryx : {
+        score : 0,
+        confirm : false
+    },
+    full : {
+        score : 0,
+        confirm : false
+    },
+    maly: {
+        score : 0,
+        confirm : false
+    },
+    duzy: {
+        score : 0,
+        confirm : false
+    },
+    general : {
+        score : 0,
+        confirm : false
+    },
+    szansa : {
+        score : 0,
+        confirm : false
+    },
     sumax2: 0,
-    razem : 0,
+    razem : 0
 }
 
 let user2Points = {
-    jedynki : 0,
-    dwojki : 0,
-    trojki : 0,
-    czworki : 0,
-    piatki : 0,
-    szostki : 0,
+    jedynki : {
+        score : 0,
+        confirm : false
+    },
+    dwojki : {
+        score : 0,
+        confirm : false
+    },
+    trojki : {
+        score : 0,
+        confirm : false
+    },
+    czworki : {
+        score : 0,
+        confirm : false
+    },
+    piatki : {
+        score : 0,
+        confirm : false
+    },
+    szostki : {
+        score : 0,
+        confirm : false
+    },
     sumax1 : 0,
-    trzyx : 0,
-    czteryx : 0,
-    full : 0,
-    maly: 0,
-    duzy: 0,
-    general : 0,
-    szansa : 0,
+    trzyx : {
+        score : 0,
+        confirm : false
+    },
+    czteryx : {
+        score : 0,
+        confirm : false
+    },
+    full : {
+        score : 0,
+        confirm : false
+    },
+    maly: {
+        score : 0,
+        confirm : false
+    },
+    duzy: {
+        score : 0,
+        confirm : false
+    },
+    general : {
+        score : 0,
+        confirm : false
+    },
+    szansa : {
+        score : 0,
+        confirm : false
+    },
     sumax2: 0,
-    razem : 0,
+    razem : 0
 }
 
 let user3Points = {
-    jedynki : 0,
-    dwojki : 0,
-    trojki : 0,
-    czworki : 0,
-    piatki : 0,
-    szostki : 0,
+    jedynki : {
+        score : 0,
+        confirm : false
+    },
+    dwojki : {
+        score : 0,
+        confirm : false
+    },
+    trojki : {
+        score : 0,
+        confirm : false
+    },
+    czworki : {
+        score : 0,
+        confirm : false
+    },
+    piatki : {
+        score : 0,
+        confirm : false
+    },
+    szostki : {
+        score : 0,
+        confirm : false
+    },
     sumax1 : 0,
-    trzyx : 0,
-    czteryx : 0,
-    full : 0,
-    maly: 0,
-    duzy: 0,
-    general : 0,
-    szansa : 0,
+    trzyx : {
+        score : 0,
+        confirm : false
+    },
+    czteryx : {
+        score : 0,
+        confirm : false
+    },
+    full : {
+        score : 0,
+        confirm : false
+    },
+    maly: {
+        score : 0,
+        confirm : false
+    },
+    duzy: {
+        score : 0,
+        confirm : false
+    },
+    general : {
+        score : 0,
+        confirm : false
+    },
+    szansa : {
+        score : 0,
+        confirm : false
+    },
     sumax2: 0,
-    razem : 0,
+    razem : 0
 }
 
 let user4Points = {
-    jedynki : 0,
-    dwojki : 0,
-    trojki : 0,
-    czworki : 0,
-    piatki : 0,
-    szostki : 0,
+    jedynki : {
+        score : 0,
+        confirm : false
+    },
+    dwojki : {
+        score : 0,
+        confirm : false
+    },
+    trojki : {
+        score : 0,
+        confirm : false
+    },
+    czworki : {
+        score : 0,
+        confirm : false
+    },
+    piatki : {
+        score : 0,
+        confirm : false
+    },
+    szostki : {
+        score : 0,
+        confirm : false
+    },
     sumax1 : 0,
-    trzyx : 0,
-    czteryx : 0,
-    full : 0,
-    maly: 0,
-    duzy: 0,
-    general : 0,
-    szansa : 0,
+    trzyx : {
+        score : 0,
+        confirm : false
+    },
+    czteryx : {
+        score : 0,
+        confirm : false
+    },
+    full : {
+        score : 0,
+        confirm : false
+    },
+    maly: {
+        score : 0,
+        confirm : false
+    },
+    duzy: {
+        score : 0,
+        confirm : false
+    },
+    general : {
+        score : 0,
+        confirm : false
+    },
+    szansa : {
+        score : 0,
+        confirm : false
+    },
     sumax2: 0,
-    razem : 0,
+    razem : 0
 }
+
+
+let round = 0;
 
 start.addEventListener("click", (e) => {
     
@@ -296,6 +456,8 @@ start.addEventListener("click", (e) => {
 })
 
 function StartGame(user){
+
+    // round++;
 
     let Game = new Promise(resolve => {
         let score = FirstRole(user)
@@ -323,110 +485,407 @@ function FirstRole(user){
 
 }
 
-// let s = [];
+function randomScore(){
+    round++;
+    if(round <= 13){
+        let s = [];
 
-// for(let i = 0; i < 5 ; i++){
-//     let r = Math.ceil(Math.random() * 6)
-//     s.push(r)
-// }
+        for(let i = 0; i < 5 ; i++){
+            let r = Math.ceil(Math.random() * 6)
+            s.push(r)
+        }
 
-// CountScore(s,user1)
+        CountScore(s,user2)
+    }
+    else{
+        console.log(user2Points)
+        console.log("koniec")
+    }
+}
 
+
+randomScore()
+
+//  zliczanie punktów
 
 function CountScore(score,user){
 
-    user = user.dataset.user;
+    let counting = new Promise(resolve => {
 
-    let tablica = document.querySelectorAll(`[data-player="${user}"]`);
+        let jedynki = score.filter(i => i == 1)
+        jedynki != 0 ? jedynki = jedynki.reduce((a,b) => a + b) : jedynki = 0;
 
-    console.log(tablica)
+        let dwojki = score.filter(i => i == 2)
+        dwojki != 0 ? dwojki = dwojki.reduce((a,b) => a + b) : dwojki = 0;
 
-    let jedynki = score.filter(i => i == 1)
-    jedynki != 0 ? jedynki = jedynki.reduce((a,b) => a + b) : jedynki = 0;
+        let trojki = score.filter(i => i == 3)
+        trojki != 0 ? trojki = trojki.reduce((a,b) => a + b) : trojki = 0;
 
-    let dwojki = score.filter(i => i == 2)
-    dwojki != 0 ? dwojki = dwojki.reduce((a,b) => a + b) : dwojki = 0;
+        let czworki = score.filter(i => i == 4)
+        czworki != 0 ? czworki = czworki.reduce((a,b) => a + b) : czworki = 0;
 
-    let trojki = score.filter(i => i == 3)
-    trojki != 0 ? trojki = trojki.reduce((a,b) => a + b) : trojki = 0;
+        let piatki = score.filter(i => i == 5)
+        piatki != 0 ? piatki = piatki.reduce((a,b) => a + b) : piatki = 0;
 
-    let czworki = score.filter(i => i == 4)
-    czworki != 0 ? czworki = czworki.reduce((a,b) => a + b) : czworki = 0;
+        let szostki = score.filter(i => i == 6)
+        szostki != 0 ? szostki = szostki.reduce((a,b) => a + b) : szostki = 0;
 
-    let piatki = score.filter(i => i == 5)
-    piatki != 0 ? piatki = piatki.reduce((a,b) => a + b) : piatki = 0;
+        let trzyx = 0;
+        let czteryx = 0;
+        let full = 0;
 
-    let szostki = score.filter(i => i == 6)
-    szostki != 0 ? szostki = szostki.reduce((a,b) => a + b) : szostki = 0;
+            let set = new Set(score)
+            let t = []
+            set.forEach(s => t.push(s))
 
-    let trzyx = 0;
-    let czteryx = 0;
-    let full = 0;
+            if(t.length <= 3){
+                t.map(x => {
 
-        let set = new Set(score)
-        let t = []
-        set.forEach(s => t.push(s))
+                    let c = 0;
 
-        if(t.length <= 3){
-            t.map(x => {
-
-                let c = 0;
-
-                score.map(i => {
-                    if(i == x){
-                        c++;
+                    score.map(i => {
+                        if(i == x){
+                            c++;
+                        }
+                    })
+                    if(t.length == 2 && c == 3){
+                        full = 25;
                     }
+
+                    if(c == 3){
+                        trzyx = score.reduce((a,b) => a + b) 
+                    }else if(c == 4){
+                        czteryx = score.reduce((a,b) => a + b) 
+                    }
+
                 })
-                if(t.length == 2 && c == 3){
-                    full = 25;
-                }
+            }
 
-                if(c == 3){
-                    trzyx = score.reduce((a,b) => a + b) 
-                }else if(c == 4){
-                    czteryx = score.reduce((a,b) => a + b) 
-                }
+        let maly = 0;
+        let duzy = 0;
 
+            if(t.length >= 4){
+
+                let sort = [...t]
+                sort = sort.sort((a,b) => a - b)
+                
+                let malycheck = [
+                    [sort.includes(1),sort.includes(2),sort.includes(3),sort.includes(4)].every(a => a),
+                    [sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5)].every(a => a),
+                    [sort.includes(3),sort.includes(4),sort.includes(5),sort.includes(6)].every(a => a),
+                ];
+
+                let duzycheck = [
+                    [sort.includes(1),sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5)].every(a => a),
+                    [sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5),sort.includes(6)].every(a => a),
+                ];
+
+                if(malycheck.some(a => a)){
+                    maly = 30;
+                }
+                if(duzycheck.some(a => a)){
+                    duzy = 40;
+                }
+                
+            }
+
+        let general = 0;
+
+            if(t.length == 1){
+
+                general = 50;
+
+            }
+
+        let szansa = score.reduce((a,b) => a + b);
+
+
+        let table = [
+            jedynki,
+            dwojki,
+            trojki,
+            czworki,
+            piatki,
+            szostki,
+            trzyx,
+            czteryx,
+            full,
+            maly,
+            duzy,
+            general,
+            szansa
+        ]
+
+        resolve(table)
+
+    })
+
+//  ustawianie punktow dla danego gracza
+
+    counting.then(r => {
+
+
+        user = user.dataset.user;
+
+        let tablica = document.querySelectorAll(`[data-player="${user}"]`);
+
+        switch(user){
+
+            case "1" :
+                SetPoints(tablica,user1Points,r,user)
+            ;break;
+            case "2" :
+                SetPoints(tablica,user2Points,r,user)
+            ;break;
+            case "3" :
+                SetPoints(tablica,user3Points,r,user)
+            ;break;
+            case "4" :
+                SetPoints(tablica,user4Points,r,user)
+            ;break;
+        }
+
+    })
+
+    
+}
+
+
+function SetPoints(tablica,userPoints,score,user){
+
+    let tablePoints = []
+    // wpisywanie w td punktow
+
+    let createTable = new Promise(resolve => {
+
+        tablica.forEach(t => {
+            if(t.classList == "points"){
+                tablePoints.push(t)
+            }
+        })
+
+
+        resolve(tablePoints)
+    })
+
+    createTable.then(r => {
+        score.forEach((s,b) => {
+            if(userPoints[r[b].dataset.cat].confirm == false){
+                r[b].innerHTML = s;
+            }
+        })
+    })
+
+    // zatwierdzanie punktów
+
+    const confirmPoints = (e) => {
+
+        let confirm = new Promise(resolve => {
+            let value = Number(e.path[0].innerHTML)
+            let cat = e.path[0].dataset.cat;
+
+            userPoints[cat].score = value;
+            userPoints[cat].confirm = true;
+
+
+            tablica.forEach(t => {
+                t.removeEventListener("click", confirmPoints)
+            })
+            resolve(tablica)
+        })
+
+
+        confirm.then(r => {
+
+            e.path[0].style.backgroundColor = "rgba(0, 0, 0, 0.603)"
+
+            clearAndSumTable(r,userPoints) 
+
+        })
+
+        confirm.then(r => {
+            randomScore()
+        })
+        
+
+    }
+
+
+    // zatwierdzanie punktów dla botów
+
+    const BotConfirmPoints = () => {
+
+        let findMaxPoint = new Promise(resolve => {
+            let sort = [...score];
+            sort = sort.sort((a,b) => a - b).reverse();
+            let pos = 0;
+            let max = sort[pos];
+            let index;
+
+            let findIndex = (call = false) => {
+
+                if(call == false){
+                    index = score.indexOf(max)
+                }else if(call == true){
+                    index = call;
+                }   
+
+                if(max == 0){
+
+                    let f = false;
+                    let i = 0;
+                    // while(!f){
+                    //     Number(tablePoints[i].innerHTML) == 0 ? f = true : i++;
+                    // }
+
+                    findIndex(index = i)
+
+                }else if(userPoints[tablePoints[index].dataset.cat].confirm == true){
+                    pos++;
+                    max = sort[pos];
+                    findIndex();
+                }else{
+                    let value = Number(tablePoints[index].innerHTML)
+                    let cat = tablePoints[index].dataset.cat;
+                    console.log(value,"value")
+                    userPoints[cat].score = value;
+                    userPoints[cat].confirm = true;
+                }
+            }
+
+            findIndex()
+
+            
+
+            resolve(tablePoints[index])
+
+        })
+
+        findMaxPoint.then(r => {
+
+            r.style.backgroundColor = "rgba(0, 0, 0, 0.603)"
+            clearAndSumTable(tablica,userPoints) ;
+
+        });
+
+        findMaxPoint.then(r => {
+            setTimeout(() => {
+                randomScore()
+            }, 2000);
+        })
+
+
+    }
+
+
+
+    // ustawanie lisenera na klikniecie danego td by zatwierdzic punkty
+
+    createTable.then(r => {
+
+        if(user == "1"){
+            r.forEach(x => {
+                if(userPoints[x.dataset.cat].confirm == false){
+                    x.addEventListener("click", confirmPoints)
+                }
             })
         }
-
-    let maly = 0;
-    let duzy = 0;
-
-        if(t.length >= 4){
-
-            let sort = [...t]
-            sort = sort.sort((a,b) => a - b)
-            
-            let malycheck = [
-                [sort.includes(1),sort.includes(2),sort.includes(3),sort.includes(4)].every(a => a),
-                [sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5)].every(a => a),
-                [sort.includes(3),sort.includes(4),sort.includes(5),sort.includes(6)].every(a => a),
-            ];
-
-            let duzycheck = [
-                [sort.includes(1),sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5)].every(a => a),
-                [sort.includes(2),sort.includes(3),sort.includes(4),sort.includes(5),sort.includes(6)].every(a => a),
-            ];
-
-            if(malycheck.some(a => a)){
-                maly = 30;
-            }
-            if(duzycheck.some(a => a)){
-                duzy = 40;
-            }
-            
+        else{
+            BotConfirmPoints()
         }
+        
 
-    let general = 0;
+    })
 
-        if(t.length == 1){
 
-            general = 50;
+    
 
+}
+
+// czyszczenie tabeli z td które nie zostały klikniete
+
+
+function clearAndSumTable(r,userPoints){
+
+    r.forEach(t => {
+        if(userPoints[t.dataset.cat].confirm == false){
+            t.innerHTML = "";
         }
+    })
 
-     let szansa = score.reduce((a,b) => a + b);
+    // obliczanie sum i łącznej sumy
 
-    console.log(score,jedynki,dwojki,trojki,czworki,piatki,szostki,trzyx,czteryx,full,maly,duzy,general,szansa)
+    let setSuma = new Promise(resolve => {
+
+        let setSumax1 = new Promise(resolve => {
+
+            userPoints["sumax1"] = userPoints["jedynki"].score + 
+            userPoints["dwojki"].score + 
+            userPoints["trojki"].score + 
+            userPoints["czworki"].score +
+            userPoints["piatki"].score +
+            userPoints["szostki"].score;
+    
+            resolve(userPoints["sumax1"])
+    
+        })
+    
+        setSumax1.then(res => {
+    
+            r.forEach(t => {
+                if(t.dataset.cat == "sumax1"){
+                    t.innerHTML = res;
+                }
+            })
+    
+        })
+        
+        let setSumax2 = new Promise(resolve => {
+    
+            userPoints["sumax2"] = userPoints["trzyx"].score + 
+            userPoints["czteryx"].score + 
+            userPoints["full"].score + 
+            userPoints["maly"].score +
+            userPoints["duzy"].score +
+            userPoints["szansa"].score;
+    
+            resolve(userPoints["sumax2"])
+    
+        })
+    
+        setSumax2.then(res => {
+    
+            r.forEach(t => {
+                if(t.dataset.cat == "sumax2"){
+                    t.innerHTML = res;
+                }
+            })
+    
+        })
+
+        resolve(userPoints)
+
+    })
+
+
+    setSuma.then(rr => {
+        let setrazem = new Promise(resolve => {
+
+            rr["razem"] = rr["sumax1"] + rr["sumax2"];
+    
+            resolve(rr["razem"])
+    
+        })
+    
+        setrazem.then(res => {
+
+            r.forEach(t => {
+                if(t.dataset.cat == "razem"){
+                    t.innerHTML = res;
+                }
+            })
+    
+        })
+    })
+
 }
